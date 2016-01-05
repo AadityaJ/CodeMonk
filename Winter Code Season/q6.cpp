30beacd853cefb97e4a0a02647591357ce1b7c65
AadityaJ <aadityajamuar@gmail.com>
2016-01-05 23:32:54 +0530

    adding files

--- Winter Code Season/q6.cpp
+++ Winter Code Season/q6.cpp
@@ -1,14 +1,21 @@
+//segment tree is involved :D
 #include <stdio.h>
-#include <stack>
 using namespace std;
-int ncr(int a,int b){
-	stack <int> st;
-	int prod=1;
-	for(int i=1;i<=a;i++){
-		st.push(i);
-	}
-	for(int i=1;i<=b;i++){
-		prod*=st.top();
-		st.pop();
-	}
+# define MAX 100 // assuming we need first 100 rows
+long long triangle[MAX + 1][MAX + 1];
+void makeTriangle() {
+    int i, j;
+
+    // initialize the first row
+    triangle[0][0] = 1; // C(0, 0) = 1
+
+    for(i = 1; i < MAX; i++) {
+        triangle[i][0] = 1; // C(i, 0) = 1
+        for(j = 1; j <= i; j++) {
+            triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+        }
+    }
+}
+long long C(int n, int r) {
+    return triangle[n][r];
 }
